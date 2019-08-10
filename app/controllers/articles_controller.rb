fddb44806b37c6968466b144ee0ad357fb5fdb20
masuteru <k.kishimoto50@gmail.com>
2019-08-10 23:19:28 +0900

    articleのedit, show, destroyアクションを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -15,16 +17,25 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+  
+  def show
+  end
 
   def edit
   end
 
-  def show
-  end
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end  
 
   private
   
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
