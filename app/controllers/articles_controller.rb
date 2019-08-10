8783aff7036354fd57610746c868f7d7129a16d6
masuteru <k.kishimoto50@gmail.com>
2019-08-10 22:08:25 +0900

    action_textを使って記事の投稿機能を追加
    ・articleモデルのcontentカラムを削除
    （action_textを使って実装するため）

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,19 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = current_user.articles.build
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -8,6 +22,9 @@ class ArticlesController < ApplicationController
   def show
   end
 
-  def new
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
--- db/migrate/20190810074210_create_articles.rb
+++ db/migrate/20190810074210_create_articles.rb
@@ -2,7 +2,6 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title,    null: false
-      t.text :content,    null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
