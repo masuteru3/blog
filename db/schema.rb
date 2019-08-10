8a409915667d2d5dc6e0cfd99967f549eb3463ac
masuteru <k.kishimoto50@gmail.com>
2019-08-10 16:45:50 +0900

    articleモデルを作成 & リレーションを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_072556) do
+ActiveRecord::Schema.define(version: 2019_08_10_074210) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_08_10_072556) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
