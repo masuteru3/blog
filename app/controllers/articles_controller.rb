c890ea2dee38ece255dc01679edd8ea426f67fea
masuteru <k.kishimoto50@gmail.com>
2019-08-11 19:02:05 +0900

    記事一覧での表示順序を変更
    新しい記事が上になるように

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.order(created_at: 'DESC')
   end
 
   def new
