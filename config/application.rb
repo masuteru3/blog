a629c7f16b1c29a7d9feb21d95c2755437f3d343
masuteru <k.kishimoto50@gmail.com>
2019-08-15 01:11:34 +0900

    日時の表示を日本標準時に変更

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Blog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
   end
 end
