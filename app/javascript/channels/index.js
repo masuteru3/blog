b59decc71a5eb848747d6cbe8db837c0fdbafd53
masuteru <k.kishimoto50@gmail.com>
2019-08-18 11:34:48 +0900

    コメントアウトメッセージを日本語に変換

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,5 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
+//このディレクトリとすべてのサブディレクトリ内のすべてのチャネルをロードします。
+//チャンネルファイルの名前は* _channel.jsでなければなりません。
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
